f437c29ce7897a6125ab913990686c17d00fd9c1
Shubham Verma <subham.varma@reachmobile.com>
2024-05-04 16:00:40 +0530

    footer added

--- components/ui/shared/Footer.tsx
+++ components/ui/shared/Footer.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import logo from '../../../public/assets/images/logo.svg'
 
 const Footer = () => {
+  const getCurrentYear: number = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image src={logo} alt="logo" height={38} width={128}/>
+        </Link>
+        <p>{getCurrentYear} Evently. All Rights Reserved</p>
+      </div>
+    </footer>
   )
 }
 
